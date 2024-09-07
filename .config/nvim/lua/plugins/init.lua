396272924a9f2c5a5f051f0c57de1463776d17c0
Brian Atlas <atlaskerr@gmail.com>
2024-09-07 11:16:02 -0500

    feat: add jsonnet

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -13,42 +13,42 @@ return {
         'preservim/nerdtree',
         lazy = false,
         config = function()
-        -- Key mappings
-        vim.api.nvim_set_keymap("n", "<C-n>", ":NERDTreeToggle<CR>", { noremap = true, silent = true })
-        --vim.api.nvim_set_keymap("n", "<Leader>n", ":NERDTreeToggle<CR>", { noremap = true, silent = true })
-        --vim.api.nvim_set_keymap("n", "<Leader>f", ":NERDTreeFind<CR>", { noremap = true, silent = true })
+            -- Key mappings
+            vim.api.nvim_set_keymap("n", "<C-n>", ":NERDTreeToggle<CR>", { noremap = true, silent = true })
+            --vim.api.nvim_set_keymap("n", "<Leader>n", ":NERDTreeToggle<CR>", { noremap = true, silent = true })
+            --vim.api.nvim_set_keymap("n", "<Leader>f", ":NERDTreeFind<CR>", { noremap = true, silent = true })
 
-        -- NERDTree settings
-        vim.g.NERDTreeShowHidden = 1
-        vim.g.NERDTreeIgnore = { '\\.vim$', '\\~$', '\\.git$', '.DS_Store' }
-        -- vim.g.NERDTreeDirArrowExpandable = ''
-        -- vim.g.NERDTreeDirArrowCollapsible = ''
+            -- NERDTree settings
+            vim.g.NERDTreeShowHidden = 1
+            vim.g.NERDTreeIgnore = { '\\.vim$', '\\~$', '\\.git$', '.DS_Store' }
+            -- vim.g.NERDTreeDirArrowExpandable = ''
+            -- vim.g.NERDTreeDirArrowCollapsible = ''
 
-        -- Autocommands
-        vim.api.nvim_create_autocmd("StdinReadPre", {
-            pattern = "*",
-            callback = function()
-            vim.g.std_in = 1
-            end,
-        })
+            -- Autocommands
+            vim.api.nvim_create_autocmd("StdinReadPre", {
+                pattern = "*",
+                callback = function()
+                    vim.g.std_in = 1
+                end,
+            })
 
-        vim.api.nvim_create_autocmd("VimEnter", {
-            pattern = "*",
-            callback = function()
-            if vim.fn.argc() == 0 and not vim.g.std_in then
-                vim.cmd("NERDTree")
-            end
-            end,
-        })
+            vim.api.nvim_create_autocmd("VimEnter", {
+                pattern = "*",
+                callback = function()
+                    if vim.fn.argc() == 0 and not vim.g.std_in then
+                        vim.cmd("NERDTree")
+                    end
+                end,
+            })
 
-        vim.api.nvim_create_autocmd("BufEnter", {
-            pattern = "*",
-            callback = function()
-            if vim.fn.winnr("$") == 1 and vim.b.NERDTreeType == "primary" then
-                vim.cmd("q")
-            end
-            end,
-        })
+            vim.api.nvim_create_autocmd("BufEnter", {
+                pattern = "*",
+                callback = function()
+                    if vim.fn.winnr("$") == 1 and vim.b.NERDTreeType == "primary" then
+                        vim.cmd("q")
+                    end
+                end,
+            })
         end,
     },
 
@@ -63,19 +63,23 @@ return {
     { 'hrsh7th/cmp-cmdline' },
     { 'hrsh7th/nvim-cmp' },
 
-    { 
+    {
         'L3MON4D3/LuaSnip',
         tag = 'v2.3.0',
         run = 'make install_jsregexp'
     },
 
-    {'saadparwaiz1/cmp_luasnip'},
+    { 'saadparwaiz1/cmp_luasnip' },
 
     -- vim-go
     {
         "fatih/vim-go",
         lazy = true,
-        ft = {'go'},
+        ft = { 'go' },
+    },
+    {
+        'google/vim-jsonnet',
+        lazy = false,
     },
 
 }
